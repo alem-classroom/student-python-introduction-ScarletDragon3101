4feb5f4db7461dfbfbf772fe7562c36a6ab6ad1b
ScarletDragon3101 <ayan1bos@gmail.com>
2020-10-01 10:42:04 +0600

    Update lists.py

--- lists/lists.py
+++ lists/lists.py
@@ -1,17 +1,36 @@
-def size_of_list(list):
-    # return size of list
+def size_of_list(list_a):
+	return len(list_a)
 
-def add_elem_to_list(list, elem):
-    # add elem to list and return the list
 
-def delete_elem_from_list(list, index = -1):
-    # delete element from list, such that its index is index
+# return size of list
 
-def count_elements_in_list(list, x):
-    # count elements in the list that are equal to x and return the count
+def add_elem_to_list(list_a, elem):
+	list_a.append(elem)
+	return list_a
 
-def sort_list(list):
-    # return sorted list
 
-def reverse(list):
-    # return reversed list
+# add elem to list and return the list
+
+def delete_elem_from_list(list_a, index=-1):
+	list_a.pop(index)
+	return list_a
+
+
+# delete element from list, such that its index is index
+
+def count_elements_in_list(list_a, x):
+	return list_a.count(x)
+
+
+# count elements in the list that are equal to x and return the count
+
+def sort_list(list_a):
+	list_a.sort()
+	return list_a
+
+
+# return sorted list
+
+def reverse(list_a):
+	list_a.reverse()
+	return list_a
