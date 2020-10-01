1ffc59f0be4a4e1da818c75844c1bd0aaadbc5c6
Ayan <62964136+ScarletDragon3101@users.noreply.github.com>
2020-10-01 11:21:17 +0600

    Update calculator.py

--- variables-and-types/calculator.py
+++ variables-and-types/calculator.py
@@ -3,15 +3,19 @@ def add(first, second):
     return first + second
 
 def sub(first, second):
+    return first - second
     # return subtraction of first and second arguments
 
 def multiply(first, second):
+    return first * second
     # return multiplicatiom of first and second arguments
 
 def div_with_floor(first, second):
+    return first // second
     # return division with floor of first and second arguments
 
 
 def div_without_floor(first, second):
+    return first / second
     # return division without floor of first and second arguments
 
