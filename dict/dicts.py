bb6224e83cb5b7a338605747fbcb58c26c24b101
Ayan <62964136+ScarletDragon3101@users.noreply.github.com>
2020-10-01 11:16:13 +0600

    Update dicts.py

--- dict/dicts.py
+++ dict/dicts.py
@@ -1,14 +1,23 @@
-def clear_dict(dict):
-    # delete everything in dict and return it
+def clear_dict(dict1):
+	dict1.clear()
+	return dict1
+
+# delete everything in dict and return it
 
 def get_dict_items(dict):
-    # return keys and values of dict
+	return dict.items()
+# return keys and values of dict
 
 def get_dict_keys(dict):
-    # return keys of dict
+	return dict.keys()
+
+# return keys of dict
 
 def get_dict_value_by_key(dict, key):
-    # return values of dict that is stored in key
+	return dict.get(key)
+
+# return values of dict that is stored in key
 
 def delete_dict_element_by_key(dict, key):
-    # delete and element from dict, such that its key is the argument key
+	dict.pop(key)
+	return dict
