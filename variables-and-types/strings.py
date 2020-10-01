68c5174ce2743bc186a98069ca78ae46ec3d40c8
Ayan <62964136+ScarletDragon3101@users.noreply.github.com>
2020-10-01 11:32:40 +0600

    Update strings.py

--- variables-and-types/strings.py
+++ variables-and-types/strings.py
@@ -1,15 +1,29 @@
 def size_of_str(str):
-    # return size of string
+	return len(str)
+
+# return size of string
 
 def concat_strings(first, second):
-    # return concatination of first and second strings
+	return first + second
+
+
+# return concatination of first and second strings
 
 def duplicate_string(str, copy):
-    # return new string which is copy of str copy times
-    # example -> duplicate_string('s', 2) == 'ss'
+	str *= copy
+	return str
+
+
+# return new string which is copy of str copy times
+# example -> duplicate_string('s', 2) == 'ss'
 
 def reverse(str):
-    # return reverse of the string
+	return str[::-1]
+
+
+# return reverse of the string
 
 def is_substring(str, substr):
-    # return true if substr is the substring of str, false otherwise
+	if substr in str:
+		return True
+	else: return False
